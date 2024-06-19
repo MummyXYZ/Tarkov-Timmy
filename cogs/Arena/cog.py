ff2fc0d9971a979a8123163a350abeafe61c1ebe
MummyXYZ <MummyXYZ@gmail.com>
2024-06-19 11:41:11 -0400

    Add Arena maps

---
+++ cogs/Arena/cog.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+import discord
+import json
+from discord import app_commands
+from discord.ext import commands
+from discord.app_commands import Choice
+
+from .subcommands.map import mapSC
+
+
+@app_commands.allowed_installs(guilds=True, users=True)
+@app_commands.allowed_contexts(guilds=True, dms=False, private_channels=True)
+class Arena(commands.GroupCog, name="arena"):
+    def __init__(self, bot: commands.AutoShardedBot) -> None:
+        self.bot = bot
+        super().__init__()
+
+    maps = []
+    with open("./configs/conf.json", "r") as f:
+        conf = json.load(f)
+
+    for location in conf["arena_Locations"]:
+        maps.append(Choice(name=location, value=location))
+
+    @app_commands.command(
+        name="map",
+        description="Maps and information for EFT: Arena",
+        # Help description of the command
+        extras=[
+            """Displays maps and information for maps on EFT: Arena.
+            
+            **E.g.** </map:1241780138593616025> <Customs>\n(*<> are required*)""",
+        ],
+    )
+    @app_commands.describe(map="Specify the map", hidden="Hide message?")
+    @app_commands.choices(map=maps)
+    @app_commands.allowed_installs(guilds=True, users=True)
+    @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+    async def map(
+        self, interaction: discord.Interaction, map: str, hidden: bool = True
+    ) -> None:
+        await mapSC.map(interaction, map, hidden)
+
+
+async def setup(bot: commands.AutoShardedBot) -> None:
+    await bot.add_cog(Arena(bot))
---
+++ cogs/Arena/subcommands/map.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+
+import discord
+import json
+from discord import Embed
+from utils.embedbuilder import embedbuilder as EB
+import logging
+import logging.handlers
+
+logger = logging.getLogger("discord")
+
+
+class mapSC:
+    async def map(interaction: discord.Interaction, map, hidden) -> None:
+        await interaction.response.defer(ephemeral=hidden)
+
+        # Load the configuration file
+        with open("./configs/conf.json", "r") as f:
+            conf = json.load(f)
+
+        image = f"{conf['arena_Locations'][map]['base']}/revision/latest/scale-to-width-down/800"
+
+        url = f"https://escapefromtarkov.fandom.com/wiki/Arena#{map.replace(" ", "_")}"
+
+        embed: Embed = EB(
+            title=map,
+            title_url=url,
+            description=f"[**Hi-res Here**]({conf['arena_Locations'][map]['base']})",
+            image_url=image,
+            footer="Deletes in 5 mins",
+        )
+
+        message: discord.Message = await interaction.followup.send(
+            embed=embed,
+            ephemeral=True,
+        )
+        await message.delete(delay=300)
