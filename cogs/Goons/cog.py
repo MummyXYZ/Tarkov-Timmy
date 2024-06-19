2b08f1d1347d734332d0687d88b433aedf2ce1a1
MummyXYZ <MummyXYZ@gmail.com>
2024-06-19 10:53:42 -0400

    Persistent View

--- cogs/Goons/cog.py
+++ cogs/Goons/cog.py
@@ -1,12 +1,58 @@
 from __future__ import annotations
 
 import discord
-import random
 import json
+from itertools import cycle
 from datetime import datetime, timedelta
 from discord import app_commands, Embed
 from discord.ext import commands
 from utils.embedbuilder import embedbuilder as EB
+
+images = cycle(["big_pipe.png", "birdeye.png", "knight.png"])
+
+
+def goonsEmbed():
+    with open("./configs/data/goons.json", "r") as f:
+        conf = json.load(f)
+
+    timestamp = int(
+        (
+            datetime.strptime(conf["Time"][0], "%B %d, %Y, %I:%M %p")
+            + timedelta(hours=1)
+        ).timestamp()
+    )
+
+    file = discord.File("./assets/" + next(images), filename="image.png")
+
+    embed: Embed = EB(
+        title="Goons Sightings",
+        description="The most recent Goons sighting.",
+        thumbnail="attachment://image.png",
+        footer="Powered by TarkovPal.com",
+        footer_icon="https://tarkovpal.com/logov2.png",
+    )
+
+    embed.add_field(
+        name="Map",
+        value=conf["Current Map"][0],
+    )
+    embed.add_field(name="Last Seen", value=f"<t:{timestamp}:t>")
+
+    return (embed, file)
+
+
+class GoonsView(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    @discord.ui.button(
+        label="Refresh",
+        style=discord.ButtonStyle.green,
+        custom_id="goons_view:refresh",
+    )
+    async def green(self, interaction: discord.Interaction, button: discord.ui.Button):
+        embed, file = goonsEmbed()
+        await interaction.response.edit_message(embed=embed, view=self)
 
 
 class Goons(commands.Cog):
@@ -29,34 +75,8 @@ class Goons(commands.Cog):
     async def map(self, interaction: discord.Interaction):
         await interaction.response.defer()
 
-        with open("./configs/data/goons.json", "r") as f:
-            conf = json.load(f)
-
-        timestamp = int(
-            (
-                datetime.strptime(conf["Time"][0], "%B %d, %Y, %I:%M %p")
-                + timedelta(hours=1)
-            ).timestamp()
-        )
-
-        images = ["big_pipe.png", "birdeye.png", "knight.png"]
-        file = discord.File("./assets/" + random.choice(images), filename="image.png")
-
-        embed: Embed = EB(
-            title="Goons Sightings",
-            description="The most recent Goons sighting.",
-            thumbnail="attachment://image.png",
-            footer="Powered by TarkovPal.com",
-            footer_icon="https://tarkovpal.com/logov2.png",
-        )
-
-        embed.add_field(
-            name="Map",
-            value=conf["Current Map"][0],
-        )
-        embed.add_field(name="Last Seen", value=f"<t:{timestamp}:t>")
-
-        await interaction.followup.send(file=file, embed=embed)
+        embed, file = goonsEmbed()
+        await interaction.followup.send(embed=embed, file=file, view=GoonsView())
 
 
 async def setup(bot: commands.AutoShardedBot) -> None:
